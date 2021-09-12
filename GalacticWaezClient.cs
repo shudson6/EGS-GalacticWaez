948abf15389e64303ae39491b05067edf73f3d1a
Steven Hudson <shudson6@asu.edu>
2021-09-12 09:36:30 -0400

    Add project files.

---
+++ GalacticWaezClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eleon.Modding;
+
+namespace GalacticWaez
+{
+    public class GalacticWaezClient : IMod
+    {
+        IModApi modApi;
+
+        public void Init(IModApi modApi)
+        {
+            this.modApi = modApi;
+            modApi.Log("GalacticWaez attached.");
+        }
+
+        public void Shutdown()
+        {
+            modApi.Log("GalacticWaez detached.");
+        }
+    }
+}
