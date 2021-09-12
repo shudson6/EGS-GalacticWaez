5ea174f35f360dc218062f32c18c3d0390d68ee6
Steven Hudson <shudson6@asu.edu>
2021-09-12 11:18:05 -0400

    Listens for commands, interprets "/waez init"

--- GalacticWaezClient.cs
+++ GalacticWaezClient.cs
@@ -3,23 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eleon;
 using Eleon.Modding;
 
 namespace GalacticWaez
 {
     public class GalacticWaezClient : IMod
     {
+        const string CommandTrigger = "/waez";
+        const string InitCommand = "init";
+
         IModApi modApi;
 
         public void Init(IModApi modApi)
         {
             this.modApi = modApi;
-            modApi.Log("GalacticWaez attached.");
+            modApi.GameEvent += onGameEvent;
+            modApi.Log("GalacticWaezClient attached.");
         }
 
         public void Shutdown()
         {
-            modApi.Log("GalacticWaez detached.");
+            modApi.GameEvent -= onGameEvent;
+            modApi.Application.ChatMessageSent -= onChatMessageSent;
+            modApi.Log("GalacticWaezClient detached.");
+        }
+
+        void onGameEvent(GameEventType type,
+                        object arg1 = null,
+                        object arg2 = null,
+                        object arg3 = null,
+                        object arg4 = null,
+                        object arg5 = null
+        ) {
+            switch (type)
+            {
+                case GameEventType.GameStarted:
+                    if (modApi.Application.Mode == ApplicationMode.SinglePlayer)
+                    {
+                        modApi.Application.ChatMessageSent += onChatMessageSent;
+                        modApi.Log("Listening for commands.");
+                    }
+                    break;
+
+                case GameEventType.GameEnded:
+                    modApi.Application.ChatMessageSent -= onChatMessageSent;
+                    modApi.Log("Stopped listening for commands.");
+                    break;
+            }
+        }
+
+        void onChatMessageSent(MessageData messageData)
+        {
+            if (messageData.Text.StartsWith(CommandTrigger))
+            {
+                string command = messageData.Text.Substring(CommandTrigger.Length).Trim();
+                if (command.Equals(InitCommand))
+                {
+                    modApi.Log("Initializing galactic highway map...");
+                    // TODO: begin memory scan
+                }
+            }
         }
     }
 }
