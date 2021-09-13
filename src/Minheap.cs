128a9b54f327df392760040370d9e0e619f79275
Steven Hudson <shudson6@asu.edu>
2021-09-13 18:34:31 -0400

    add Minheap

---
+++ src/Minheap.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+namespace GalacticWaez.src
+{
+    class Minheap<ItemType>
+    {
+        class Node
+        {
+            public readonly ItemType Item;
+            public readonly float Priority;
+            public Node(ItemType item, float priority)
+            {
+                Item = item;
+                Priority = priority;
+            }
+        }
+
+        private List<Node> nodes;
+
+        public int Count { get => nodes.Count; }
+
+        public Minheap()
+        {
+            nodes = new List<Node>();
+        }
+
+        public void Insert(ItemType item, float priority)
+        {
+            nodes.Add(new Node(item, priority));
+            Bubble(Count - 1);
+        }
+
+        public ItemType RemoveMin()
+        {
+            if (Count < 1) return default;
+            ItemType min = nodes[0].Item;
+            int index = Count - 1;
+            nodes[0] = nodes[index];
+            nodes.RemoveAt(index);
+            Sink();
+            return min;
+        }
+
+        public void Clear()
+        {
+            nodes.Clear();
+        }
+
+        void Bubble(int index)
+        {
+            if (index > 0 && index < Count)
+            {
+                int parent = Parent(index);
+                if (nodes[index].Priority.CompareTo(nodes[parent].Priority) < 0)
+                {
+                    swap(index, parent);
+                    Bubble(parent);
+                }
+            }
+        }
+
+        void Sink(int index = 0)
+        {
+            int left = Left(index);
+            int right = Right(index);
+            int child;
+
+            if (left >= Count) return;
+            if (right < Count
+                && nodes[right].Priority.CompareTo(nodes[left].Priority) < 0)
+            {
+                child = right;
+            }
+            else child = left;
+
+            if (nodes[index].Priority.CompareTo(nodes[child].Priority) > 0)
+            {
+                swap(index, child);
+                Sink(child);
+            }
+        }
+
+        int Left(int index) => 2 * index + 1;
+        int Right(int index) => 2 * index + 2;
+        int Parent(int index) => (index + 1) / 2 - 1;
+
+        void swap(int a, int b)
+        {
+            Node temp = nodes[a];
+            nodes[a] = nodes[b];
+            nodes[b] = temp;
+        }
+    }
+}
