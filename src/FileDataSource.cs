2b3584ac0d305773d383baf38a53543aade07f41
Steven Hudson <shudson6@asu.edu>
2021-09-29 18:40:09 -0400

    add FileDataSource and first tests

---
+++ src/FileDataSource.cs
@@ -0,0 +1,63 @@
+﻿using Eleon.Modding;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GalacticWaez
+{
+    public class FileDataSource : IGalaxyDataSource
+    {
+        public const string ModContentDir = "Content\\Mods\\GalacticWaez";
+        private const string FileName = "stardata.csv";
+
+        public readonly string PathToFile;
+        private readonly LoggingDelegate Log;
+
+        public FileDataSource(string saveGameDir, LoggingDelegate logger)
+        {
+            Log = logger;
+            PathToFile = $"{saveGameDir}\\{ModContentDir}\\{FileName}";
+        }
+
+        public IEnumerable<VectorInt3> GetGalaxyData()
+        {
+            if (!File.Exists(PathToFile))
+            {
+                Log("Star data not present in save file.");
+                return null;
+            }
+
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(new FileStream(
+                    PathToFile, FileMode.Open, FileAccess.Read
+                    ));
+
+                int count = int.Parse(reader.ReadLine());
+                var starPositions = new List<VectorInt3>(count);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var coords = line.Split(separator: ",".ToCharArray(), count: 3);
+                    starPositions.Add(new VectorInt3(
+                        int.Parse(coords[0]),
+                        int.Parse(coords[1]),
+                        int.Parse(coords[2])
+                        ));
+                }
+                return (count == starPositions.Count) ? starPositions : null;
+            }
+            catch (Exception ex)
+            {
+                Log("Exception while loading star data: ");
+                Log(ex.Message);
+                return null;
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+    }
+}
