f6c068abfaa57e679cf21d4be5d7bfc91793036e
Steven Hudson <shudson6@asu.edu>
2021-09-29 18:40:09 -0400

    add FileDataSource and first tests

---
+++ test/FileDataSourceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GalacticWaez;
+
+namespace GalacticWaezTests
+{
+    [TestClass]
+    [DeploymentItem("Dependencies\\stardata-test-small.csv")]
+    public class FileDataSourceTests
+    {
+        [TestMethod]
+        public void PathToFile_Correct()
+        {
+            const string expected = "foo\\bar\\Content\\Mods\\GalacticWaez\\stardata.csv";
+            Assert.AreEqual(expected, new FileDataSource("foo\\bar", null).PathToFile);
+        }
+
+        [TestMethod]
+        public void ReturnsNull_WhenFileNotExists()
+        {
+            Assert.IsNull(new FileDataSource("foo\\bar", (_) => { }).GetGalaxyData());
+        }
+    }
+}
