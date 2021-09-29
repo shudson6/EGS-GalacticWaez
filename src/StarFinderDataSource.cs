9eb4cc1e58d0cdb8ae0ae24bdb66d8ebd96066f3
Steven Hudson <shudson6@asu.edu>
2021-09-29 19:30:46 -0400

    add StarFinderDataSource

---
+++ src/StarFinderDataSource.cs
@@ -0,0 +1,15 @@
+﻿using Eleon.Modding;
+using System.Collections.Generic;
+
+namespace GalacticWaez
+{
+    class StarFinderDataSource : IGalaxyDataSource
+    {
+        private readonly ISaveGameDB db;
+
+        public StarFinderDataSource(ISaveGameDB db) { this.db = db; }
+
+        public IEnumerable<VectorInt3> GetGalaxyData()
+            => new StarFinder().Search(db.GetFirstKnownStarPosition());
+    }
+}
