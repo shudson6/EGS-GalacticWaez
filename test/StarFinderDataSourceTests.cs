54198df45091fc69cacb72ce5b765f8f589d6b5c
Steven Hudson <shudson6@asu.edu>
2021-09-30 10:34:36 -0400

    add large (reforged eden) galaxy test data

--- test/StarFinderDataSourceTests.cs
+++ test/StarFinderDataSourceTests.cs
@@ -8,6 +8,7 @@ namespace GalacticWaezTests
 {
     [TestClass]
     [DeploymentItem("Dependencies\\stardata-test-vanilla.csv")]
+    [DeploymentItem("Dependencies\\stardata-test-large.csv")]
     public class StarFinderDataSourceTests
     {
         [TestMethod]
@@ -38,6 +39,27 @@ namespace GalacticWaezTests
             });
         }
 
+        [TestMethod]
+        public void ReturnsExpected_LargeGalaxy()
+        {
+            // running this test in its own domain.
+            // because StarFinder will scan app memory, it needs to be isolated from other tests
+            // which might contaminate it.
+            var testDomain = AppDomain.CreateDomain("Test Domain: ReturnsExpected_VanillaData_LogNull",
+                AppDomain.CurrentDomain.Evidence, AppDomain.CurrentDomain.SetupInformation);
+            testDomain.DoCallBack(() =>
+            {
+                var gameData = GalaxyTestData.LoadGameStyleStarArray("stardata-test-large.csv");
+                var expected = GalaxyTestData.LoadPositions("stardata-test-large.csv");
+                var actual = new StarFinderDataSource(new KnownVectorDB(expected[0]), (_) => { })
+                    .GetGalaxyData();
+                Assert.IsTrue(gameData.Count > 0);
+                Assert.IsTrue(expected.Count > 0);
+                Assert.AreEqual(expected.Count, actual.Count());
+                Assert.IsFalse(expected.Except(actual).Any());
+            });
+        }
+        
         [TestMethod]
         public void ReturnsNull_WhenDbNull()
         {
