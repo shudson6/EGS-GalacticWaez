5e8f46d5ec4490d4ba9d56e438ccc185e9c4777c
Steven Hudson <shudson6@asu.edu>
2021-10-01 22:03:27 -0400

    more bookmarkmanager tests

--- src/IBookmarkManager.cs
+++ src/IBookmarkManager.cs
@@ -16,19 +16,21 @@
         /// Retrieves coordinates for a requested bookmark, if it exists and belongs to
         /// the specified player or faction. In case it belongs to the faction but not the
         /// player, its isshared attribute must be set.
-        /// 
+        /// <br/>
         /// If the bookmark is retrieved, <c>coordinates</c> is set accordingly and return
         /// is <c>true</c>. If not, <c>coordinates</c> is <c>default(VectorInt3)</c> and
         /// return is <c>false.</c>
         /// </summary>
         /// <param name="playerId"></param>
         /// <param name="playerFacId"></param>
-        /// <param name="bookmarkName"></param>
+        /// <param name="bookmarkName">must not be <c>null</c></param>
         /// <param name="coordinates"></param>
         /// <returns>
         /// <c>true</c> on successful retrieval
+        /// <br/>
         /// <c>false</c> otherwise
         /// </returns>
+        /// <exception cref="System.ArgumentNullException">if bookmarkname is null</exception>
         bool TryGetVector(int playerId, int playerFacId, string bookmarkName,
             out Eleon.Modding.VectorInt3 coordinates);
     }
--- test/BookmarkManagerTests.cs
+++ test/BookmarkManagerTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mono.Data.Sqlite;
 using System;
-using System.Data;
 using System.IO;
 using Eleon.Modding;
 using GalacticWaez;
@@ -95,6 +94,113 @@ namespace GalacticWaezTests
         public void Throws_IfSaveGameDirNull()
         {
             var _ = new BookmarkManager(null, delegate { });
+        }
+
+        [TestMethod]
+        public void Succeed_IfFactionAndShared()
+        {
+            var sql = GetConnection(true);
+            var cmd = sql.CreateCommand();
+            var expected = new VectorInt3(300000, 400000, 500000);
+            cmd.CommandText = "insert into Bookmarks"
+                + " values (1, 0, 0, 1, 1337, 418, 42, 'Test Star',"
+                + $"{expected.x}, {expected.y}, {expected.z}, 0, 0, 0,"
+                // isshared is the second entry on this row
+                + " 2, 1, 0, 0, 1, 0, 131071, 0, 0, -1);";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            sql.Dispose();
+
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            Assert.IsTrue(bm.TryGetVector(1337, 1337, "Test Star", out VectorInt3 actual));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Fail_IfFactionButNotShared()
+        {
+            var sql = GetConnection(true);
+            var cmd = sql.CreateCommand();
+            var vector = new VectorInt3(300000, 400000, 500000);
+            cmd.CommandText = "insert into Bookmarks"
+                + " values (1, 0, 0, 1, 1337, 418, 42, 'Test Star',"
+                + $"{vector.x}, {vector.y}, {vector.z}, 0, 0, 0,"
+                // isshared is the second entry on this row
+                + " 2, 0, 0, 0, 1, 0, 131071, 0, 0, -1);";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            sql.Dispose();
+
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            Assert.IsFalse(bm.TryGetVector(1337, 1337, "Test Star", out VectorInt3 actual));
+            Assert.AreEqual(default(VectorInt3), actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Throw_IfBookmarkNameNull()
+        {
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            bm.TryGetVector(0, 0, null, out var _);
+        }
+
+        [TestMethod]
+        public void Fail_IfPlayerNorFactionMatch()
+        {
+            var sql = GetConnection(true);
+            var cmd = sql.CreateCommand();
+            var vector = new VectorInt3(300000, 400000, 500000);
+            cmd.CommandText = "insert into Bookmarks"
+                + " values (1, 0, 0, 1, 1337, 418, 42, 'Test Star',"
+                + $"{vector.x}, {vector.y}, {vector.z}, 0, 0, 0,"
+                + " 2, 0, 0, 0, 1, 0, 131071, 0, 0, -1);";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            sql.Dispose();
+
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            Assert.IsFalse(bm.TryGetVector(404, 404, "Test Star", out VectorInt3 actual));
+            Assert.AreEqual(default(VectorInt3), actual);
+        }
+
+        [TestMethod]
+        public void Fail_IfPlayerNorFactionMatch_EvenIfShared()
+        {
+            var sql = GetConnection(true);
+            var cmd = sql.CreateCommand();
+            var vector = new VectorInt3(300000, 400000, 500000);
+            cmd.CommandText = "insert into Bookmarks"
+                + " values (1, 0, 0, 1, 1337, 418, 42, 'Test Star',"
+                + $"{vector.x}, {vector.y}, {vector.z}, 0, 0, 0,"
+                // isshared is the second entry on this row
+                + " 2, 1, 0, 0, 1, 0, 131071, 0, 0, -1);";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            sql.Dispose();
+
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            Assert.IsFalse(bm.TryGetVector(404, 404, "Test Star", out VectorInt3 actual));
+            Assert.AreEqual(default(VectorInt3), actual);
+        }
+
+        [TestMethod]
+        public void Succeed_IfPlayerButNotFaction()
+        {
+            // i don't actually think this can happen
+            var sql = GetConnection(true);
+            var cmd = sql.CreateCommand();
+            var expected = new VectorInt3(300000, 400000, 500000);
+            cmd.CommandText = "insert into Bookmarks"
+                + " values (1, 0, 0, 1, 418, 1337, 42, 'Test Star',"
+                + $"{expected.x}, {expected.y}, {expected.z}, 0, 0, 0,"
+                + " 2, 1, 0, 0, 1, 0, 131071, 0, 0, -1);";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            sql.Dispose();
+
+            var bm = new BookmarkManager(deploymentDir, delegate { });
+            Assert.IsTrue(bm.TryGetVector(1337, 1337, "Test Star", out VectorInt3 actual));
+            Assert.AreEqual(expected, actual);
         }
 
         private static SqliteConnection GetConnection(bool writeable)
