dc283122dad1a4d04f113e145f66a57665998669
Steven Hudson <shudson6@asu.edu>
2021-10-04 18:23:23 -0400

    save star data after successful scan

---
+++ src/Interface/IGalaxyStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SectorCoordinates = Eleon.Modding.VectorInt3;
+
+namespace GalacticWaez
+{
+    public interface IGalaxyStorage
+    {
+        /// <summary>
+        /// Writes star positions to storage.
+        /// </summary>
+        /// <param name="positions">The collection of star positions to be written.</param>
+        /// <returns>
+        /// <c>true</c> on success, <c>false</c> on failure
+        /// </returns>
+        bool StoreGalaxyData(IEnumerable<SectorCoordinates> positions);
+    }
+}
---
+++ test/Fakes/FakeStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Eleon.Modding;
+using GalacticWaez;
+
+namespace GalacticWaezTests.Fakes
+{
+    class FakeStorage : IGalaxyStorage
+    {
+        public bool StoreGalaxyData(IEnumerable<VectorInt3> positions)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
