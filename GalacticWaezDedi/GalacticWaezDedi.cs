085b3dbceb813c23bf0c5c3a22fe7d0f7158ad59
Steven Hudson <shudson6@asu.edu>
2021-10-07 09:06:40 -0400

    add first dedi files

---
+++ GalacticWaezDedi/GalacticWaezDedi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eleon.Modding;
+
+namespace GalacticWaez.Dedi
+{
+    public class GalacticWaezDedi : IMod
+    {
+        public void Init(IModApi modAPI)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
